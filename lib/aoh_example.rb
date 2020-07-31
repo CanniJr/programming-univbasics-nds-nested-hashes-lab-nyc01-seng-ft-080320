30d6f75dd170ef425aafc2d0d592f370898c88eb
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-31 01:27:18 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,7 +21,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  nested_array [
+  nested_array = [
     DON_G,
     JOELLE_VD,
     PAT_M,
